7f5aa30a7767ceff60c8cf856658fe45e2cac0fd
Adam von Arnim <vonarnim@usc.edu>
2020-05-01 11:42:00 -0400

    insert fix

--- hw6/bst-avl/bst-test.cpp
+++ hw6/bst-avl/bst-test.cpp
@@ -5,14 +5,13 @@
 
 using namespace std;
 
-
 int main(int argc, char *argv[])
 {
     // Binary Search Tree tests
     BinarySearchTree<char,int> bt;
     bt.insert(std::make_pair('a',1));
     bt.insert(std::make_pair('b',2));
-    
+
     cout << "Binary Search Tree contents:" << endl;
     for(AVLTree<char,int>::iterator it = bt.begin(); it != bt.end(); ++it) {
         cout << it->first << " " << it->second << endl;
