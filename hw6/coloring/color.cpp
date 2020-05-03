7593cae46416fcd72d8b3628d72461d9e5509f86
Adam von Arnim <vonarnim@usc.edu>
2020-05-03 13:05:40 -0400

    cMap indexing

--- hw6/coloring/color.cpp
+++ hw6/coloring/color.cpp
@@ -22,6 +22,8 @@ int main(int argc, char* argv[])
     ifstream in_file(argv[2]);
     ofstream out_file(argv[3]);
 
+    std::cout << "files opened" << std::endl;
+
     int numCountries;
     int rows;
     int cols;
@@ -29,12 +31,20 @@ int main(int argc, char* argv[])
     std::set<char> countryList;
     std::map<char, std::set<char>> connections; //used for tracking countries to which a contry is connected
 
+    std::cout << "variables created" << std::endl;
+
     in_file >> numCountries >> rows >> cols;
+    std::cout << "variables initialized" << std::endl;
+
     //this size will allow easy comparisons between upper/lower/left/right grids
     char** cMap = new char*[rows+2];
-    for (int i = 1; i < rows+1; i++)
+    for (int i = 0; i < rows+2; i++)
     {
       cMap[i] = new char[cols+2];
+    }
+    // filling the map
+    for (int i = 1; i < rows+1; i++)
+    {
       for (int j = 1; j < cols+1; j++)
       {
         in_file >> cMap[i][j];
@@ -42,16 +52,36 @@ int main(int argc, char* argv[])
       }
       in_file >> holdNextLineChar;
     }
+    std::cout << "cMap created" << std::endl;
+
+    for (std::set<char>::iterator it = countryList.begin(); it != countryList.end(); ++it)
+    {
+      std::cout << "printing countryList" << std::endl;
+      std::cout << *it << std::endl;
+    }
+
     //assigning outer edges of chars in the map to null equivalent
+    std::cout << "filling cMap" << std::endl;
     for (int i = 0; i < cols+2; i++)
       {
+        std::cout << "here" << std::endl;
         cMap[0][i] = '\0';
+        std::cout << "here" << std::endl;
         cMap[rows+1][i] = '\0';
       }
     for (int  i = 0; i < rows+2; i++)
     {
       cMap[i][0] = '\0';
       cMap[i][cols+1] = '\0';
+    }
+    std::cout << "printing cMap" << std::endl;
+    for (int i = 0; i < rows+2; i++)
+    {
+      for (int j = 0; j < cols+2; j++)
+      {
+        std::cout << cMap[i][j];
+      }
+      std::cout << std::endl;
     }
 
     AVLTree<char, int> coloring;
@@ -61,6 +91,7 @@ int main(int argc, char* argv[])
       connections.insert(std::make_pair(*countryIt, std::set<char>()));
       coloring.insert(std::make_pair(*countryIt, 0));
     }
+    std::cout << "filled coloring, countryIt" << std::endl;
     //make connections
     for (int i = 1; i < rows+2; i++)
     {
@@ -77,16 +108,26 @@ int main(int argc, char* argv[])
           connections[current].insert(cMap[i+1][j]);
       }
     }
+    std::cout << "printing all nodes in connections" << std::endl;
+    for (std::map<char, std::set<char>>::iterator it = connections.begin(); it != connections.end(); ++it)
+    {
+      std::cout << it->first << std::endl;
+    }
     determineColoring(connections, coloring, 0, out_file);
 
     // Call printSolution to output the answers to the output file
 
+    for (int i = 0; i < rows+2; i++)
+    {
+      delete [] cMap[i];
+    }
+    delete [] cMap;
     return 0;
 }
 
 bool determineColoring(std::map<char, std::set<char>>& links, AVLTree<char, int>& coloring, int idx, std::ostream& os)
 {
-  if (idx == links.size())  //if every country has been assigned a color, print the solution
+  if ((unsigned int) idx == links.size())  //if every country has been assigned a color, print the solution
   {
     printSolution(coloring, os);
     return true;
