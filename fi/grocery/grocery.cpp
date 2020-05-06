138c32ddd05444ba99339bcb1f4d0610de79027b
Adam von Arnim <vonarnim@usc.edu>
2020-05-06 13:30:39 -0400

    groc

--- fi/grocery/grocery.cpp
+++ fi/grocery/grocery.cpp
@@ -11,6 +11,41 @@
 std::vector<GItem> maxGroceryBox(const std::vector<GItem>& inventory, int weight_limit)
 {
     // TODO implement!
+    std::vector<GItem> results;
+    return constructBox(inventory, results, weight_limit, 0);
+
+}
+
+std::vector<GItem> constructBox(std::vector<GItem> inventory, std::vector<GItem> holdings, int weight_limit, int idx)
+{
+  if (getWeight(box) >= weight_limit && idx < inventory.size())
+    return holdings;
+  std::vector<GItem> resultWO = constructBox(inventory, holdings, weight_limit, idx + 1);
+  holdings.push_back(inventory[idx]);
+  std::vector<GItem> resultW = constructBox(inventory, holdings, weight_limit, idx + 1);
+  if (getValue(resultWO) > getValue(resultW)) //return max of two options
+    return resultWO;
+  else
+    return resultW;
+}
 
 
+int getWeight(const std::vector<GItem> box)
+{
+  int totalW = 0;
+  for (int i = 0; i < box.size(); i++)
+  {
+    totalW += box[i].weight;
+  }
+  return totalW;
+}
+
+int getValue(const std::vector<GItem> box)
+{
+  int value = 0;
+  for (int i = 0; i < box.size(); i++)
+  {
+    value += box[i].value;
+  }
+  return value;
 }
